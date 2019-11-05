fb54c42f9590b1c8d59457618eb0297fd2e32b03
Tom Oberhauser <tom@devfoo.de>
2019-11-05 16:24:58 +0100

    WIP

--- count_definition_languages.py
+++ count_definition_languages.py
@@ -8,12 +8,10 @@ from tqdm import tqdm
 from UMLSParser import UMLSParser
 from langdetect import detect
 
-umls = UMLSParser('/home/toberhauser/DEV/Data/UMLS/2019AA-full/2019AA', language_filter=['GER'])
-# beer = umls.get_concepts()['C0004922']
-# print(beer.cui, beer.get_preferred_names_for_language('GER'), beer.get_preferred_names_for_language('ENG'))
-
+umls = UMLSParser('/home/toberhauser/DEV/Data/UMLS/2019AA-full/2019AA', language_filter=['SPA', 'GER'])
 
 language_counter = collections.defaultdict(collections.Counter)
+
 
 def langdetect_thread(definition: str):
     try:
@@ -23,15 +21,10 @@ def langdetect_thread(definition: str):
     language_counter[source].update({lang: 1})
 
 
-for cui, concept in tqdm(list(umls.get_concepts().items())[:1000]):
+for cui, concept in tqdm(umls.get_concepts().items()):
     for definition, source in concept.get_definitions():
         if len(definition) > 0:
             langdetect_thread(definition)
-            # thread = threading.Thread(target=langdetect_thread, args=(definition,))
-            # threads.append(thread)
-            # thread.start()
-
-# [x.join() for x in threads]
 
 for source, languages in language_counter.items():
     print(source, languages)
